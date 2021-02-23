1f587d4e2dbbc5a9d495578e14e205bc21bdd678
Johannes <johannesjandke@gmail.com>
2021-02-23 11:30:28 +0100

    Added power_on.py

--- power_on.py
+++ power_on.py
@@ -1,12 +1,33 @@
-#!/usr/bin/env python
+# /usr/bin/python3
+# -*- coding: utf-8 -*-
+# from https://github.com/JJandke
+#
 
 from datetime import datetime
-import logging
 import RPi.GPIO as GPIO
+import logging
+import os
 
+# Create log file for this script
+# Running the script via cronjob at boot will delete the log file each time to save disk space and keep the file more organized.
+if os.path.isfile("/home/pi/log/piled/power_on.log"):
+    os.remove("/home/pi/log/piled/power_on.log")
+    f = open("/home/pi/log/piled/power_on.log", "x")
+    f.close()
+else:
+    f = open("/home/pi/log/piled/power_on.log", "x")
+    f.close()
+
+logging.basicConfig(filename="/home/pi/log/piled/power_on.log", level=logging.DEBUG)
 now = datetime.now()
+log_time = now.strftime("%a-%d.%m.%Y-%H:%M:%S ")
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(12,GPIO.OUT)
-GPIO.output(12,GPIO.LOW)
+try:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(12,GPIO.OUT)
+    GPIO.output(12,GPIO.LOW)
+    logging.info("{0}Power On".format(log_time))
+
+except Exception as e:
+    logging.debug("{0}Could not power On: {1}".format(log_time, e))
