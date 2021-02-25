4e4fd0008e9dead0d2a87a1bb44f9bff7bd1da5d
Johannes <johannesjandke@gmail.com>
2021-02-25 10:25:55 +0100

    Added turquoise.py

---
+++ turquoise.py
@@ -0,0 +1,39 @@
+# /usr/bin/python3
+# -*- coding: utf-8 -*-
+# from https://github.com/JJandke
+#
+
+from datetime import datetime
+from time import sleep
+import logging
+import pigpio
+import os
+
+# Create log file if it does not exist.
+if not os.path.isfile("/home/pi/log/piled.log"):
+    f = open("/home/pi/log/piled.log", "x")
+    f.close()
+else:
+    pass
+
+logging.basicConfig(filename="/home/pi/log/piled.log", level=logging.DEBUG)
+pi = pigpio.pi()
+now = datetime.now()
+log_time = now.strftime("%a-%d.%m.%Y-%H:%M:%S ")
+
+# It checks if the files exist. If they do, they will be deleted. If they do not exist, fade.py or strobe.py will stop if they are running.
+try:
+    if os.path.exists("/home/pi/.server/fade.txt"):
+        os.remove("/home/pi/.server/fade.txt")
+    if os.path.exists("/home/pi/.server/multistrobe.txt"):
+        os.remove("/home/pi/.server/multistrobe.txt")
+
+    # Waiting one second is necessary, otherwise requested color will be overwritten by the last fade command.
+    sleep(1)
+    pi.set_PWM_dutycycle(16, 0)     # red
+    pi.set_PWM_dutycycle(20, 250)   # green
+    pi.set_PWM_dutycycle(21, 150)   # blue
+    logging.info("{0}Set color: Turquoise".format(log_time))
+
+except Exception as e:
+    logging.debug("{0}Could not set color to Turquoise: {1}".format(log_time, e))
