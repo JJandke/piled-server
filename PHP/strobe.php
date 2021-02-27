e27e9b7f48b8a81d3670c65b578df1a61df690ac
Johannes <johannesjandke@gmail.com>
2021-02-27 21:12:35 +0100

    Better documentation in strobe.php

--- PHP/strobe.php
+++ PHP/strobe.php
@@ -1,5 +1,8 @@
 <?php
 
+/* The script is called strobe.php although it does not call strobe.py but multistrobe.py.
+But since strobe.py is not used, I have the name available and can use it in the URL, so the URL won't be so long. */
+
 $command = escapeshellcmd('/home/pi/Code/App/strobe.py');
 $output = shell_exec($command);
 echo $output;
