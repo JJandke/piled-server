2f213481fe4e6aa0e7ed660d30fcb9e2247f39f3
Johannes <johannesjandke@gmail.com>
2022-12-29 22:04:51 +0100

    updated poweron.php

--- PHP/poweron.php
+++ PHP/poweron.php
@@ -1,6 +1,6 @@
 <?php
 
-$command = escapeshellcmd('/home/pi/Code/App/power_on.py');
+$command = escapeshellcmd('/home/pi/code/python/power_on.py');
 $output = shell_exec($command);
 echo $output;
 
