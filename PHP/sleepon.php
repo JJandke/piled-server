b8a7c866192a5efaf3bad79e2fa60b04f31f7558
Johannes <johannesjandke@gmail.com>
2022-12-29 22:06:16 +0100

    updated sleepon.php

--- PHP/sleepon.php
+++ PHP/sleepon.php
@@ -1,6 +1,6 @@
 <?php
 
-$command = escapeshellcmd('/home/pi/Code/App/sleep_on.py');
+$command = escapeshellcmd('/home/pi/code/python/sleep_on.py');
 $output = shell_exec($command);
 echo $output;
 
